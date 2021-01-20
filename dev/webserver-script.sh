ec159a4146c8be45c5f8f260d4f7e55485797332
kapil <kapil.kumaria>
2021-01-20 20:38:40 +0000

    modified webserver-script.sh

--- dev/webserver-script.sh
+++ dev/webserver-script.sh
@@ -16,6 +16,8 @@ service apache2 start
 service apache2 enable
 cp dir.conf /etc/apache2/mods-enabled/dir.conf
 cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
+cd /var/www/html
+mkdir Dev-Project1
 cp ../index.js /var/www/html/Dev-Project1/index.js
 cp ../index.test.js /var/www/html/Dev-Project1/index.test.js
 cp ../package.json /var/www/html/Dev-Project1/package.json
