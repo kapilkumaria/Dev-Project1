ae9e9657d1517da7aaf5db10d3d7fe37cceb9dff
kapil <kapil.kumaria>
2021-01-20 20:25:28 +0000

    modified webserver-script.sh

--- dev/webserver-script.sh
+++ dev/webserver-script.sh
@@ -16,11 +16,11 @@ service apache2 start
 service apache2 enable
 cp dir.conf /etc/apache2/mods-enabled/dir.conf
 cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
+cp ../index.js /var/www/html/Dev-Project1/index.js
+cp ../index.test.js /var/www/html/Dev-Project1/index.test.js
+cp ../package.json /var/www/html/Dev-Project1/package.json
 apt install nodejs -y
 apt install npm -y
 npm install
-cd /var/www/html
-git clone https://github.com/kapilkumaria/Dev-Project1.git
-cd Dev-Project1
 service apache2 start
 npm start
