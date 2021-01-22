2316fecff6541c58d59fb0c9f8df44aebb075b7c
kapil <kapil.kumaria>
2021-01-22 05:17:15 +0000

    modified ec2 user-data

--- dev/webserver-script.sh
+++ dev/webserver-script.sh
@@ -14,16 +14,14 @@ apt update -y
 apt install apache2 -y
 service apache2 start
 service apache2 enable
-cp dir.conf /etc/apache2/mods-enabled/dir.conf
-cp 000-default.conf /etc/apache2/sites-enabled/000-default.conf
 mkdir /var/www/html/Dev-Project1
-cp /var/lib/jenkins/workspace/kk1/dev/index.js /var/www/html/Dev-Project1/
-cp /var/lib/jenkins/workspace/kk1/dev/index.test.js /var/www/html/Dev-Project1/
-cp /var/lib/jenkins/workspace/kk1/dev/package.json /var/www/html/Dev-Project1/
-cp /var/lib/jenkins/workspace/kk1/dev/000-default.conf /etc/apache2/sites-enabled/
-cp /var/lib/jenkins/workspace/kk1/dev/dir.conf /etc/apache2/mods-enabled/
 apt install nodejs -y
 apt install npm -y
+cp /var/lib/jenkins/workspace/kk1/dev/index.js /var/www/html/Dev-Project1/index.js
+cp /var/lib/jenkins/workspace/kk1/dev/index.test.js /var/www/html/Dev-Project1/index.test.js
+cp /var/lib/jenkins/workspace/kk1/dev/package.json /var/www/html/Dev-Project1/package.json
+cp /var/lib/jenkins/workspace/kk1/dev/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+cp /var/lib/jenkins/workspace/kk1/dev/dir.conf /etc/apache2/mods-enabled/dir.conf
 npm install
 service apache2 reload
 service apache2 restart
