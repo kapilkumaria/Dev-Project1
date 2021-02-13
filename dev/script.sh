7c04fc41a09378dec6916ca365a905403d912e76
kapil <kapil.kumaria@gmail.com>
2021-02-13 07:00:26 +0000

    modified some files

--- dev/script.sh
+++ dev/script.sh
@@ -5,7 +5,7 @@ apt install awscli -y
 apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 apt update -y
-sudo apt install openjdk-8-jdk -y
+apt install openjdk-8-jdk -y
 apt-cache policy docker-ce
 apt install docker.io -y
 service docker start
@@ -15,4 +15,3 @@ chkconfig docker on
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 931058976119.dkr.ecr.us-east-1.amazonaws.com
 docker pull 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp:latest
 docker run -d -it -p 3000:3000 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp
-
--- script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt install openjdk-8-jdk -y
-sudo groupadd docker
-sudo usermod -aG docker ubuntu
-su -s ubuntu
