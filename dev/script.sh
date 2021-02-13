96dd3b33bd59a1022f416fcbdb92d68d48d0a549
kapil <kapil.kumaria@gmail.com>
2021-02-13 05:48:37 +0000

    modified ec2 user-data

--- dev/script.sh
+++ dev/script.sh
@@ -10,6 +10,7 @@ apt-cache policy docker-ce
 apt install docker.io -y
 service docker start
 usermod -a -G docker ubuntu
+chmod 666 /var/run/docker.sock
 chkconfig docker on
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 931058976119.dkr.ecr.us-east-1.amazonaws.com
 docker pull 931058976119.dkr.ecr.us-east-1.amazonaws.com/my-nodeapp:latest
