b8f89b682c4ceb0f2061510788af65de2e6676ff
kapil <kapil.kumaria@gmail.com>
2021-02-13 19:40:08 +0000

    tried GitHub Webhook

--- dockerpermission.sh
+++ dockerpermission.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo su 
+apt upadte -y
 sudo apt install openjdk-8-jdk -y
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 931058976119.dkr.ecr.us-east-1.amazonaws.com
 docker build -t my-nodeapp .
